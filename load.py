25319363af4cdecd8b66c61517d21e1427e22de7
raouftams <raouftams@gmail.com>
2022-02-13 16:09:34 +0100

    add check functions (check vehicles, check tickets, check towns) && load functions (load rotation, insert tickets, insert rotations) && data enrichement function for vehicle and towns

--- load.py
+++ load.py
@@ -9,22 +9,203 @@ import numpy as np
 from utilities import rotations_table_types, vehicles_table_types
 
 
-#insert a rotation to the database (rotation table)
-def insert_rotation(data, db_con):
-   pass
+#insert rotations to database
+def insert_rotations(data, db_connection):
+    """
+    Args
+        data: petl dicts of rotations data
+        db_connection: psycopg2 db connection instance
+    Purpose:
+        insert rotations data to database
+    """
+    #create rotation table instance
+    rotation = RotationTable()
 
-#insert a vehicle to the database (vehicle table)
-def insert_vehicle(row):
-    pass
+    for row in data:
+        rotation_dict = {
+            "id_vehicle": row["vehicle_id"],
+            "code_ticket": row["ticket"],
+            "code_town": row["town_code"],
+            "date": row["date"],
+            "date_hijri": row["date_hijri"],
+            "heure": row["time"],
+            "cet": row["cet"]
+        }
+        #insert data
+        rotation.insert(rotation_dict, db_connection)
 
-#insert a ticket to the database (ticket table)
-def insert_ticket(row):
-    pass
+#insert ticket to database
+def insert_ticket(data, db_connection):
+    """
+    Arguments:
+        data: dict from petl table
+    Purpose:
+        insert ticket data to database
+    """
 
-#insert a town to the database (commune table)
-def insert_town(row):
-    pass
+    #create ticket table instance
+    ticket = TicketTable()
 
+    #check net wieghts
+    net = data['net_cet']
+    if net == 'nan':
+        net = data['net_extra']
+
+    #transform dict 
+    ticket_dict = {
+        "code": data['ticket'],
+        "brute": data['brute'],
+        "net": net,
+        "date": data['date'],
+        "heure": data['time'],
+    }
+    #insert data
+    ticket.insert(ticket_dict, db_connection)
+
+#insert vehicles to the database (vehicle table)
+def check_vehicle(table, db_connection):
+    """
+    Arguments:
+        table: petl table of rotations data
+    Purpose:
+        check if vehicles exist in database
+    """
+    #create vehicle table instance
+    vehicle = VehicleTable()
+
+    #extract vehicles data from table and transform into numpy array
+    vehicles_array = etl.values(table, ["vehicle_id", "vehicle_mat"])
+    #remove rows with vehicle_id = nan or vehicle_mat = nan or do not exist in the database
+    for row in vehicles_array:
+        #if vehicle data is nan
+        if row[0] == "nan" and row[1] == "nan":
+            #remove rows
+            table = etl.selectisnot(table, {"vehicle_id": row[0], "vehicle_mat": row[1]})
+        else:
+            #check if exists in database
+            #check if code exists
+            if row[0] != "nan" and not vehicle.exists(row[0], db_connection):
+                #remove row
+                table = etl.selectisnot(table, "vehicle_id", row[0])
+            else:
+                #check if mat exists
+                if row[1] != "nan" and not vehicle.exists_mat(row[1], db_connection):
+                    #remove row
+                    table = etl.selectisnot(table, "vehicle_mat", row[1])
+    
+    return table
+
+#insert tickets to the database (ticket table)
+def check_ticket(table, db_connection):
+    """
+    Arguments: 
+        table: petl table
+    Purpose:
+        check if ticket exists in database else add to db
+    """
+
+    #create ticket table instance
+    ticket = TicketTable()
+
+    #extract tickets data from table and transform into numpy array
+    tickets_array = etl.values(table, "ticket")
+    #remove rows with ticket_code = nan or does not exist in the database
+    for code_t in tickets_array:
+        #check if the ticket exists in database
+        if code_t == "nan":
+            #remove rows
+            table = etl.selectisnot(table, "ticket", code_t)
+        if not ticket.exists(code_t, db_connection):
+            #insert ticket data to database
+            #select ticket data
+            ticket_data = etl.select(table, lambda row: row["ticket"] == code_t)
+            #transform ticket data into dict
+            ticket_data = etl.dicts(ticket_data)
+            #insert ticket data to database
+            insert_ticket(ticket_data[0], db_connection)
+
+    return table
+    
+
+#insert towns to the database (commune table)
+def check_town(table, db_connection):
+    """
+    Arguments:
+        table: petl table
+        db_connection: database connection
+    Purpose:
+        check if town exists in database else remove it from petl table
+    """
+
+    #create town table instance
+    town = TownTable()
+
+    #extract towns data from table and transform into numpy array
+    towns_array = etl.values(table, ["town_code", "town"])
+    #remove rows with code_commune = nan or does not exist in the database
+    for row in towns_array:
+        code_c = row[0]
+        name_c = row[1]
+        #check if town data is nan
+        if code_c == "nan" and name_c == "nan":
+            #remove row
+            table = etl.selectisnot(table, {"town_code": code_c, "town": name_c})
+        else:
+            #check if the commune exists in database
+            #check if code exists
+            if code_c != "nan" and not town.exists(code_c, db_connection):
+                #remove rows
+                table = etl.selectisnot(table, "town_code", code_c)
+            else:
+                #check if name exists
+                if name_c != "nan" and not town.exists_name(name_c, db_connection):
+                    #remove rows
+                    table = etl.selectisnot(table, "town", name_c)
+    return table
+
+""" data enrichement """
+#enrich vehicles data
+def enrich_vehicle_data(table, db_connection):
+    """
+    Args: 
+        table: petl table of rotation data
+        db_connection: psycopg2 db connection instance
+    purpose:
+        insert vehicle code if nan based on vehicle mat
+    """
+    #create vehicle table instance
+    vehicle = VehicleTable()
+
+    table = etl.convert(
+        table, "vehicle_id", 
+        lambda v, row: vehicle.get_code_from_mat(row["vehicle_mat"], db_connection),
+        where=lambda r: r["vehicle_id"] == "nan"
+    )
+    
+    return table
+
+#enrich towns data
+def enrich_town_data(table, db_connection):
+    """
+    Args: 
+        table: petl table of rotation data
+        db_connection: psycopg2 db connection instance
+    purpose:
+        insert vehicle code if nan based on vehicle mat
+    """
+    #create town table instance
+    town = TownTable()
+
+    table = etl.convert(
+        table, "town_code", 
+        lambda v, row: town.get_code_from_name(row["town_name"], db_connection), 
+        where=lambda r: r["town_code"] == "nan"
+    )
+    
+    return table
+
+
+""" Load data """
 #load rotations data to the database
 def load_rotations(rotation_table):
     """
@@ -38,22 +219,25 @@ def load_rotations(rotation_table):
     #change columns types
     rotation_table = etl.convert(rotation_table, rotations_table_types)
 
-    #extract unities data from table and transform into numpy array
-    unities_array = etl.toarray(etl.distinct(rotation_table, ["unit", "unit_code"]))
+    #check if town exists in database
+    rotation_table = check_town(rotation_table, db_connection)
+    #chekc if ticket exists in database
+    rotation_table = check_ticket(rotation_table, db_connection)
+    #check if vehicle exists in database
+    rotation_table = check_vehicle(rotation_table, db_connection)
 
-    #create unity table instance
-    unity = UnityTable()
+    """ data enrichment """
+    #vehicle data enrichment
+    rotation_table = enrich_vehicle_data(rotation_table, db_connection)
+    #town data enrichment
+    rotation_table = enrich_town_data(rotation_table, db_connection)
 
-    for row in unities_array:
-        if row[0] != "nan" and row[1] != "nan" and not unity.exists(row[1]): 
-            #init dict
-            data_dict = {
-                "unit": row[0], 
-                "unit_code": row[1]
-                }
-            #insert data
-            unity.insert(data_dict, db_connection)
-    
+    #extract important columns from rotation table
+    rotations_data = etl.dicts(rotation_table)
+
+    #load rotations data to the database
+    insert_rotations(rotations_data, db_connection)
+
 
 #load vehicles data to database
 def load_vehicles(data):
