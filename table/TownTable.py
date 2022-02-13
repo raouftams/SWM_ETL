bac488fa3a68e678f50b83217f13ee7a06213a83
raouftams <raouftams@gmail.com>
2022-02-13 16:07:05 +0100

    add find with matricule and get code to vehicleTable && add find name and get code by name to TownTable

--- table/TownTable.py
+++ table/TownTable.py
@@ -5,3 +5,50 @@ class TownTable(Table):
     def __init__(self) -> None:
         super().__init__("commune")
     
+
+    #check if town name exists
+    def exists_name(self, name, db_connection):
+        """
+        Args
+            name: town name
+            db_connection: pysoppg2 instance
+        purpose
+            check if town name exists in database
+        """
+
+        #create cursor
+        cursor = db_connection.cursor()
+
+        #execute query
+        cursor.execute("SELECT * FROM commune WHERE name = '{}'".format(name.upper()))
+        #get result
+        result = cursor.fetchall()
+        #close cursor
+        cursor.close()
+        #check if result is empty
+        if result != []:
+            return True
+        
+        return False
+
+    #get code from name
+    def get_code_from_name(self, name, db_connection):
+        """
+        Args:
+            name: town name
+            db_connection: psycopg2 db connection instance
+        Get town's code using town's name
+        """
+        #create cursor
+        cursor = db_connection.cursor()
+        #execute query
+        cursor.execute("SELECT code FROM commune WHERE name = '{}'".format(name.upper()))
+        #get result
+        result = cursor.fetchone()
+        #close cursor
+        cursor.close()
+        #check if result is empty
+        if result != []:
+            return result[0]
+        
+        return None
--- table/VehicleTable.py
+++ table/VehicleTable.py
@@ -5,3 +5,64 @@ class VehicleTable(Table):
     def __init__(self) -> None:
         super().__init__("vehicle")
     
+
+    #check if matricule exists in database
+    def exists_mat(self, mat, db_connection):
+        """
+        Args: mat: vehicle matricule
+            db_connection: psycopg2 db instance
+        puprose: 
+            check if mat exists in vehicle table
+        """
+
+        #create cursor
+        cursor = db_connection.cursor()
+        """check if mat = nouveau_matricule"""
+        #execute query
+        cursor.execute("SELECT * FROM vehicle WHERE nouveau_matricule = '{}'".format(mat))
+        #get result
+        result = cursor.fetchall()
+        #check if result is empty
+        if result != []:
+            cursor.close()
+            return True
+        else:
+            """check if mat = ancien_matricule"""
+            cursor.execute("SELECT * FROM vehicle WHERE ancien_matricule = '{}'".format(mat))
+            result = cursor.fetchall()
+            cursor.close()
+            if result != []:
+                return True
+        
+        return False
+
+    #get code from matricule
+    def get_code_from_mat(self, mat, db_connection):
+        """
+        Args:
+            mat: vehicle matricule
+            db_connection: psycopg2 db connection instance
+        Get vehicle's code using vehicle' matricule
+        """
+        #create cursor
+        cursor = db_connection.cursor()
+        #check with nouveau_matricule
+        #execute query
+        cursor.execute("SELECT code FROM vehicle WHERE nouveau_matricule = '{}'".format(mat))
+        #get result
+        result = cursor.fetchone()
+        #check if result is empty
+        if result != []:
+            cursor.close()
+            return result[0]
+        else:
+            #check with ancien_matricule
+            cursor.execute("SELECT code FROM vehicle WHERE ancien_matricule = '{}'".format(mat))
+            result = cursor.fetchone()
+            cursor.close()
+            if result != []:
+                return result[0]
+        
+        return None
+
+
