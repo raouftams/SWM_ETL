b12959fd42637ab1baa3a9d6b67c4d30291d67dc
raouftams <raouftams@gmail.com>
2022-02-21 22:39:53 +0100

    update main

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ import petl as etl
 import pandas as pd
 import sys
 
+
 def main():
     sys.setrecursionlimit(10000)
-    path = "D:\PFE M2\data\\new_data\BDD Rotations 2018-2019.xlsx"
-    print("data extraction...")
-    data, sheets = extract_data_from_file(path)
-    table = transform_rotation_data(data, sheets)
     
-    print(table)
-    write_petl_table_to_csv(table, "out/new_data.csv")
-    #load_rotations(table)
+    path = "D:\PFE M2\data\\new_data\BDD Rotations 2018-2019.xlsx"
+    
+    table = etl.frompickle("out/rotations_table_final.pkl")
+    
+    load_rotations(table)
     print("end")
 
 
